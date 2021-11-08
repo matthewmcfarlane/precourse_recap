94bc579ba1f74fa3e1615830eb6bce79893827ee
Matthew McFarlane <matthewmcfarlane1@outlook.com>
2021-11-08 18:56:43 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,17 @@
+int_var = 10
+float_var = 5.3
+string_var = 'hello'
+string_number = '87'
+age_1 = 25
+age_2 = 26
+bool_var = True
+
+print(int_var)
+print(float_var)
+print(string_var)
+print(string_number)
+print(int(string_number))
+print(age_1 + age_2)
+print(string_var.upper())
+print(string_var.title())
+print(string_var.isalpha())
